dfe94212c0198d01e0b138a70342e034e6a1b4fc
Ben Croker <ben@putyourlightson.com>
2025-08-04 09:38:31 +0200

    Set error signals during validation

--- src/Validation/SignalValidator.php
+++ src/Validation/SignalValidator.php
@@ -5,15 +5,72 @@
 
 namespace Putyourlightson\Datastar\Validation;
 
+use Illuminate\Validation\ValidationException;
 use Illuminate\Validation\Validator;
 
 class SignalValidator extends Validator
 {
+    public const ERROR_KEY = 'errors';
+
     /**
      * @inheritdoc
      */
-    public function validate()
+    public function validate(): array
     {
-        return $this->validated();
+        $validated = [];
+
+        try {
+            $validated = parent::validate();
+            sse()->patchSignals([static::ERROR_KEY => []]);
+        } catch (ValidationException $exception) {
+            $this->sendErrorResponse($exception->errors());
+        }
+
+        return $validated;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validateWithBag(string $errorBag): array
+    {
+        $validated = [];
+
+        try {
+            $validated = parent::validate();
+            sse()->patchSignals([$errorBag => []]);
+        } catch (ValidationException $exception) {
+            $this->sendErrorResponse($exception->errors(), $exception->errorBag);
+        }
+
+        return $validated;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function validated(): array
+    {
+        $validated = [];
+
+        try {
+            $validated = parent::validated();
+            sse()->patchSignals([static::ERROR_KEY => []]);
+        } catch (ValidationException $exception) {
+            $this->sendErrorResponse($exception->errors());
+        }
+
+        return $validated;
+    }
+
+    /**
+     * Sends an error response with the validation errors.
+     */
+    private function sendErrorResponse(array $errors, string $errorBag = null): void
+    {
+        $errorKey = $errorBag ?? static::ERROR_KEY;
+        sse()->patchSignals([$errorKey => $errors])
+            ->getEventStream()
+            ->send();
     }
 }

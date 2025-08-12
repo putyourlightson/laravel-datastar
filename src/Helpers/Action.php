c060dff3fcf64d70980ddcdfb5e9be7b3986faaa
Ben Croker <ben@putyourlightson.com>
2025-08-12 20:37:42 +0200

    Make test pass

--- src/Helpers/Action.php
+++ src/Helpers/Action.php
@@ -32,9 +32,7 @@ class Action
     private static function addCsrfToken(array|string $options): string
     {
         $csrfHeader = 'X-CSRF-TOKEN';
-
-        // Get the CSRF token from the request or use an empty string if not available.
-        $token = csrf_token() ?? '';
+        $token = csrf_token();
 
         if (is_array($options)) {
             return self::addCsrfToArray($options, $csrfHeader, $token);
@@ -43,7 +41,7 @@ class Action
         return self::addCsrfToString($options, $csrfHeader, $token);
     }
 
-    private static function addCsrfToArray(array $options, string $csrfHeader, string $token): string
+    private static function addCsrfToArray(array $options, string $csrfHeader, ?string $token): string
     {
         $headers = $options['headers'] ?? [];
         $headers[$csrfHeader] = $token;
@@ -52,7 +50,7 @@ class Action
         return json_encode($options);
     }
 
-    private static function addCsrfToString(string $options, string $csrfHeader, string $token): string
+    private static function addCsrfToString(string $options, string $csrfHeader, ?string $token): string
     {
         if (preg_match('/headers:\s*\{/i', $options)) {
             return preg_replace(

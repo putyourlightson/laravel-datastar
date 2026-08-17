5b5322004d04e94f4ce7dd0194a2e109e08319f6
Ben Croker <ben@putyourlightson.com>
2026-08-17 22:37:40 +0200

    Use `request()` to read signals

--- src/Helpers/Request.php
+++ src/Helpers/Request.php
@@ -8,6 +8,7 @@ namespace Putyourlightson\Datastar\Helpers;
 use Illuminate\Contracts\Translation\Translator;
 use Illuminate\Support\Facades\Validator;
 use Putyourlightson\Datastar\Validation\SignalValidator;
+use starfederation\datastar\Consts;
 use starfederation\datastar\ServerSentEventGenerator;
 
 class Request
@@ -32,6 +33,13 @@ class Request
      */
     public static function readSignals(): array
     {
-        return ServerSentEventGenerator::readSignals();
+        if (in_array(request()->method(), ['GET', 'DELETE'], true)) {
+            $input = request()->query(Consts::DATASTAR_KEY);
+        } else {
+            $input = request()->getContent();
+        }
+        $signals = $input ? json_decode($input, true) : [];
+
+        return is_array($signals) ? $signals : [];
     }
 }
